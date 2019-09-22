a33db88a79e16a6049008f5f3c59d1c93a9e9421
theseus yang <theseusyang@192.168.0.105>
2019-09-22 11:41:21 +0800

    add annotations for mdm

--- src/main/java/com/datasphere/government/mdm/CodeValuePair.java
+++ src/main/java/com/datasphere/government/mdm/CodeValuePair.java
@@ -23,7 +23,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.datasphere.server.domain.CollectionPatch;
+import com.datasphere.server.common.domain.CollectionPatch;
 
 @Entity
 @Table(name = "mdm_code_value_pair")
